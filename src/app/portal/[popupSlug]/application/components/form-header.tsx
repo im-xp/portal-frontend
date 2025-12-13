d3d2f8673e2a2348378e0be9c67cc8aa121524da
MaryLiz Bender <maryliz@im-xp.com>
2025-12-13 15:50:55 -0600

    Updated form header image URL. Hard-coded so this is not set in the database.

--- src/app/portal/[popupSlug]/application/components/form-header.tsx
+++ src/app/portal/[popupSlug]/application/components/form-header.tsx
@@ -17,8 +17,8 @@ export function FormHeader() {
       <div className="flex items-center">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src={city.image_url}
-          alt={city.name}
+          src="https://storage.googleapis.com/icelandeclipse/portal-background5.jpg"
+          alt="The Portal at Iceland Eclipse background image"
           style={{height: 'auto'}}
           className="w-full md:min-h-[190px] md:w-[20vw] md:max-w-[240px] object-cover dark:invert rounded-2xl"
         />
